80c9faa88750ffd3855c1cc44c7684500b95ae30
pw11-085 <yogi.krsh22@gmail.com>
2023-05-23 23:41:13 +0530

    Redux Basic Structure Added

---
+++ hilling_travel_agency/src/Redux/actiontypes.js
@@ -0,0 +1,5 @@
+export const FETCH_REQUEST = 'FETCH_REQUEST';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS'
+export const FETCH_ERROR = 'FETCH_ERROR'
+export const FETCH_USER = 'FETCH_USER'
+export const FETCH_REVIEWS = 'FETCH_REVIEWS'
