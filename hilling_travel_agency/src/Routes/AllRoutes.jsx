5a609451b9df4c9094bb691de8f2f5cc4e0b02ca
ShubhamRaut187 <raut.shubham4554@gmail.com>
2023-05-23 17:57:09 +0530

    Template Added

---
+++ hilling_travel_agency/src/Routes/AllRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Routes,Route} from "react-router-dom"
+function AllRoutes(props) {
+    return (
+        <Routes>
+            {/* Add Routes here */}
+        </Routes>
+    );
+}
+
+export default AllRoutes;
